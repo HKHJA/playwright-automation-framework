2782b690fe6abcea69138adfe3759830fa969a77
HKHJA <hayder.k.hameed@gmail.com>
2026-08-24 03:12:18 +0300

    Add barrel export for page objects

---
+++ pages/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Page Object Barrel Export
+ * =========================
+ * Import all page objects from a single entry point.
+ *
+ * Usage:
+ *   import { LoginPage, InventoryPage, CartPage, CheckoutPage } from '../pages';
+ */
+
+export { LoginPage } from './LoginPage';
+export { InventoryPage } from './InventoryPage';
+export { CartPage } from './CartPage';
+export { CheckoutPage } from './CheckoutPage';
